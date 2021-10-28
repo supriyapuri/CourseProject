c3dec74fbc3baac7e40216fecb4a93ed981b97e4
jrw-7 <87761619+jrw-7@users.noreply.github.com>
2021-10-27 19:38:52 -0700

    Update page_scraper.py

--- page_scraper.py
+++ page_scraper.py
@@ -1,44 +1,49 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
-page = requests.get("https://www.rottentomatoes.com/m/nomadland")
-soup = BeautifulSoup(page.content, 'html.parser')
+#Open txt file with urls
+with open("data/movie_urls.txt") as f:
+    url = f.readlines()
 
-#"Where to watch"
-affiliate = soup.find_all("a", "affiliate__link")
-affiliateList = []
-for data in affiliate:
-    affiliateList.append(data["data-affiliate"])
+pages = []
 
-print(affiliateList)
+#Iterate through each url
+for i in url:
+    page = requests.get(i)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-#Movie Info
-synopsis = soup.find('div', attrs={'id': 'movieSynopsis'})
-synopsis = synopsis.string.strip()
+    content = ""
 
-print(synopsis)
+    #Where to watch
+    affiliate = soup.find_all("a", "affiliate__link")
+    for data in affiliate:
+        content = content + data["data-affiliate"] + " "
 
-#TODO -- Some attributes not mapping due to extra html tags
-#TODO -- Not all pages have the same movie info
-attributes = soup.find_all('div', attrs={'data-qa': 'movie-info-item-value'})
+    #Movie Info
+    synopsis = soup.find('div', attrs={'id': 'movieSynopsis'})
+    content = content + synopsis.string.strip() + " "
 
-rating = attributes[0].string.strip()
-genre = attributes[1].string.strip()
-language = attributes[2].string.strip()
-distributor = attributes[9].string.strip()
-sound = attributes[10].string.strip()
-aspectratio = attributes[11].string.strip()
-print(rating)
-print(genre)
-print(language)
-print(distributor)
-print(sound)
-print(aspectratio)
+    attributes = soup.find_all('div', attrs={'data-qa': 'movie-info-item-value'})
 
-#Cast
-castList = []
-cast = soup.find_all("span", "characters subtle smaller")
-for data in cast:
-    castList.append(data["title"])
+    rating = attributes[0].string.strip()
+    content = content + rating + " "
+    genre = attributes[1].string.strip()
+    genre = re.sub(r"\s+", " ", genre) #remove tabs and line breaks
+    content = content + genre + " "
 
-print(castList)
+    #Cast
+    cast = soup.find_all("span", "characters subtle smaller")
+    for data in cast:
+        content = content + data["title"] + " "
+
+    pages.append(content.strip())
+
+def write_lst(lst,file_):
+    with open(file_,'w') as f:
+        for l in lst:
+            f.write(l)
+            f.write('\n')
+
+output = 'data/pages.txt'
+write_lst(pages,output)
