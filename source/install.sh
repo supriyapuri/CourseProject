1ae77d36f78a763add3bb38a6517a6f33fe15a91
supriyapuri <supriyapuri11@gmail.com>
2021-11-16 20:07:48 -0800

    Add data files and update install.sh

--- source/install.sh
+++ source/install.sh
@@ -1,1 +1,10 @@
-pip install flask
+pip install Flask
+pip install metapy
+pip install pytoml
+pip install beautifulsoup4
+pip install metapy
+pip install numpy
+pip install scipy
+pip install selenium
+pip install gunicorn
+
